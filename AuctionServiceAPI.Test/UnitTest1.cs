397b55eb1a28c1e50a5bbb8b48ddac82a9fa2683
Salim Jama <eaasjam@students.eaaa.dk>
2024-05-16 13:29:15 +0200

    initial commmmmit

---
+++ AuctionServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AuctionServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

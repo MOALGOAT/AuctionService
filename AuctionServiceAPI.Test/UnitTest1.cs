8fd4fca920decdf5b6a812e677756b49e6eb9341
Salim Jama <eaasjam@students.eaaa.dk>
2024-05-16 13:57:51 +0200

    lavet flere unit test.............

--- AuctionServiceAPI.Test/UnitTest1.cs
+++ AuctionServiceAPI.Test/UnitTest1.cs
@@ -51,5 +51,84 @@ namespace AuctionServiceAPI.Tests
                 _ = auction;
             });
         }
+
+        [Test]
+        public void AuctionModel_Bids_Success()
+        {
+            // Arrange
+            var bid1 = new Bid();
+            var bid2 = new Bid();
+            var bids = new List<Bid> { bid1, bid2 };
+
+            var auction = new Auction
+            {
+                _id = 1,
+                Item = new Item(),
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddHours(1),
+                Bids = bids
+            };
+
+            // Act & Assert
+            Assert.That(auction.Bids, Is.Not.Null);
+            Assert.AreEqual(2, auction.Bids.Count);
+            Assert.Contains(bid1, auction.Bids);
+            Assert.Contains(bid2, auction.Bids);
+        }
+
+        [Test]
+        public void AuctionModel_NullItem_Failure()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var auction = new Auction
+                {
+                    _id = 1,
+                    Item = null, // Setting item to null should throw ArgumentNullException
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddHours(1),
+                    Bids = new List<Bid>()
+                };
+            });
+        }
+
+        [Test]
+        public void AuctionModel_DuplicateBids_Failure()
+        {
+            // Arrange
+            var bid1 = new Bid();
+            var bids = new List<Bid> { bid1, bid1 }; // Adding same bid twice
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var auction = new Auction
+                {
+                    _id = 1,
+                    Item = new Item(),
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddHours(1),
+                    Bids = bids
+                };
+            });
+        }
+
+        [Test]
+        public void AuctionModel_StartTimeAfterEndTime_Failure()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var auction = new Auction
+                {
+                    _id = 1,
+                    Item = new Item(),
+                    StartTime = DateTime.Now.AddHours(1), // Setting start time after end time
+                    EndTime = DateTime.Now,
+                    Bids = new List<Bid>()
+                };
+            });
+        }
     }
 }

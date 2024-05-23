87eb5fab288758da45c2c405c793b912a9d7743d
Rasmus Damgaard <rasmusdamgaard7@gmail.com>
2024-05-23 14:23:52 +0200

    public model

--- AuctionServiceAPI/Models/Item.cs
+++ AuctionServiceAPI/Models/Item.cs
@@ -5,12 +5,12 @@ namespace AuctionServiceAPI.Models
     public class Item
     {
         [BsonId]
-        Guid _id { get; set; }
+     public  Guid _id { get; set; }
      public string title { get; set; }
     public string description { get; set; }
     public string category { get; set; }
-        float startingPrice { get; set; }
-        string status { get; set; }
+    public  float startingPrice { get; set; }
+    public  string status { get; set; }
 
     }
 }

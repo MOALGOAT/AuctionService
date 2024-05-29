76d23ad1c2a5cf461ab29f43e74fa26e0bc4d3cf
jakobhaulund <jakobhaulund@hotmail.com>
2024-05-29 15:52:33 +0200

    heeheehardcode

--- AuctionServiceAPI/Program.cs
+++ AuctionServiceAPI/Program.cs
@@ -32,10 +32,9 @@ try
 
     string mySecret = await vaultService.GetSecretAsync("secrets", "SecretKey") ?? "5Jw9yT4fb9T5XrwKUz23QzA5D9BuY3p6";
     string myIssuer = await vaultService.GetSecretAsync("secrets", "IssuerKey") ?? "gAdDxQDQq7UYNxF3F8pLjVmGuU5u8g3y";
+    string myConnectionString = await vaultService.GetConnectionStringAsync("secrets", "MongoConnectionString") ?? "mongodb://admin:1234@mongodb:27017";
 
-    string connectionString = await vaultService.GetConnectionStringAsync("secrets", "MongoConnectionString");
-
-    configuration["MongoConnectionString"] = connectionString;
+    configuration["MongoConnectionString"] = myConnectionString;
     vaultService = new VaultService(configuration);
 
     builder.Services.AddEndpointsApiExplorer();
